7a5f099d065d00d9e2a90780bc9c89c606f2896a
A. Osama <Altimos@desktop.com>
2022-12-05 17:48:30 +0200

    Setup and some stylings

---
+++ src/views/BoardView.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- Board Class Styles -->
+    <div class="p-4 bg-teal-dark h-full overflow-auto">
+
+    </div>
+
+    <!-- Column Class Styles -->
+    <div class="p-2 mr-4 text-left shadow rounded bg-gray-300">
+
+    </div>
+
+    <!-- Task Class Styles -->
+    <div class="flex items-center flex-wrap shadow mb-2 py-2 px-2 rounded bg-white text-gray-900 no-underline">
+
+    </div>
+</template>
---
+++ src/views/TaskView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="relative flex flex-row bg-white pin mx-auto m-32 py-4 text-left rounded max-w-[700px] shadow">
+
+    </div>
+</template>
