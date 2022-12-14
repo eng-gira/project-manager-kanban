6ae3a0edf375bc318bcdaffbfd302e20ff14bc42
A. Osama <Altimos@desktop.com>
2022-12-14 23:43:19 +0200

    Add token to headers of col and task requests

--- src/services/ProjectService.js
+++ src/services/ProjectService.js
@@ -62,66 +62,102 @@ export default {
     },
 
     getColumns(projectId) {
-        return apiClient.get('projects/' + projectId + '/columns');
+        return apiClient.get('projects/' + projectId + '/columns', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+            }
+        });
     },
     getSingleColumn(id) {
-        return apiClient.get('columns/' + id)
+        return apiClient.get('columns/' + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')                
+            }
+        })
     },
     createColumn(projectId, data) {
         return apiClient.post('projects/' + projectId +'/columns', data, { headers: 
             {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
         })
     },
     updateColumn(id, data) {
         return apiClient.post('columns/' + id, data, { headers: 
             {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
         })        
     },
     changeColsOrder(projectId, data) {
         return apiClient.post('projects/' + projectId + '/columns/changeOrder', data, { headers: 
             {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
         })     
     },
     deleteColumn(id) {
-        return apiClient.delete('columns/' + id)         
+        return apiClient.delete('columns/' + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')                
+            }
+        })         
     },
 
 
     getTasks(columnId) {
-        return apiClient.get('columns/' + columnId + '/tasks');
+        return apiClient.get('columns/' + columnId + '/tasks', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+            }
+        });
     },
     getSingleTask(id) {
-        return apiClient.get('tasks/' + id)
+        return apiClient.get('tasks/' + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+            }
+        })
     },
     createTask(columnId, data) {
         return apiClient.post('columns/' + columnId +'/tasks', data, { headers: 
             {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
         })
     },
     updateTask(id, data) {
         return apiClient.post('tasks/' + id, data, { headers: 
             {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
         })        
     },
     relocateTask(id, data) {
         return apiClient.post('tasks/' + id + '/relocate', data, { headers: 
             {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
             }
         })        
     },
     deleteTask(id) {
-        return apiClient.delete('tasks/' + id)         
+        return apiClient.delete('tasks/' + id, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+            }
+        })         
     },
     // Members
     getMembers(projectId) {
